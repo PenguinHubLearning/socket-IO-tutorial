364899c9382439e2d269c586b4753f3f3a90c96e
Hamdi MILED <hamdi.miled@sofia.local>
2018-10-19 17:06:04 +0100

    super chat hhh

--- app.js
+++ app.js
@@ -1,37 +1,29 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    fs = require('fs');
 
-var http = require('http');
-var fs = require('fs');
-
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-// Chargement de socket.io
-var io = require('socket.io').listen(server);
-
-// Quand un client se connecte, on le note dans la console
-io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !');
-    socket.emit('message', 'Vous êtes bien connecté !');
-    socket.broadcast.emit('message', 'Un autre client vient de se connecter !');
-
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session
-    socket.on('petit_nouveau', function(pseudo) {
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        console.log(' pseudo',pseudo)
+        pseudo = ent.encode(pseudo);
+        console.log('ent pseudo',ent.encode(pseudo))
         socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un "message" (clic sur le bouton), on le note dans la console
-    socket.on('client', function (message) {
-        // On récupère le pseudo de celui qui a cliqué dans les variables de session
-        console.log(socket.pseudo + ' me parle ! Il me dit : ' + message);
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 });
 
-server.on('close', function() {
-    console.log('Server arrete ....')
-})
 server.listen(8080);
